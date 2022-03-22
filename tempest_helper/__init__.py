385124347a0c859239df9f6f9a1623f1caf2efeb
Jon Seddon <17068361+jonseddon@users.noreply.github.com>
2022-03-22 12:46:46 +0000

    alphabetical sort of imports

--- tempest_helper/__init__.py
+++ tempest_helper/__init__.py
@@ -8,12 +8,12 @@ from tempest_helper.analyse_trajectories import (
 )
 from tempest_helper.load_trajectories import get_trajectories
 from tempest_helper.plot_trajectories import plot_trajectories_cartopy
+from tempest_helper.save_trajectories import save_trajectories_netcdf
 from tempest_helper.trajectory_manipulations import (
     convert_date_to_step,
     fill_trajectory_gaps,
-    storms_overlap_in_time,
-    storms_overlap_in_space,
-    write_track_line,
     rewrite_track_file,
+    storms_overlap_in_space,
+    storms_overlap_in_time,
+    write_track_line,
 )
-from tempest_helper.save_trajectories import save_trajectories_netcdf
